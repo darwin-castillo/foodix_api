05a63631df40a7c7c87895700d27a4aab77630d0
Darwin <darwin.c5@gmail.com>
2026-03-27 23:26:50 -0400

    feat: initialize NestJS project structure with base configuration and testing setup

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+// src/app.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true, // Carga automáticamente las entidades registradas
+      synchronize: true, // Solo para desarrollo; crea las tablas automáticamente
+    }),
+  ],
+})
+export class AppModule { }
